3691bd6b60698c058386a1da65d40b9a6d638e48
Paulo Henrique <phvcandido@gmail.com>
2020-09-16 00:40:13 -0300

    Adicionando revelar cards e localstorage

--- src/pages/Play/index.vue
+++ src/pages/Play/index.vue
@@ -16,7 +16,7 @@
         @click="verifyResult(card)"
         v-for="card in cards.sort(function() { return 1 - Math.random()})"
         :key="card"
-        :style="card.isDiscovered? 'background:green' : ''"
+        :class="card.isDiscovered? 'active' : ''"
       >
         <h1 v-html="card.content"></h1>
       </div>
@@ -28,30 +28,36 @@ export default {
   name: "Play",
   data: () => ({
     cards: [],
-    card1:'',
-    card2:''
+    card1: "",
+    card2: "",
   }),
   mounted() {
-    for (var i = 0; i < 6; i++) {
-      var char1 = this.getRandomIntInclusive(0, 10);
-      var char2 = this.getRandomIntInclusive(0, 10);
+    if (localStorage.getItem("LIBRAS_MATH_CARDS_DATA")) {
+      this.cards = JSON.parse(localStorage.getItem("LIBRAS_MATH_CARDS_DATA"));
+    } else {
+      for (var i = 0; i < 6; i++) {
+        var char1 = this.getRandomIntInclusive(0, 10);
+        var char2 = this.getRandomIntInclusive(0, 10);
 
-      var result = char1 + char2;
+        var result = char1 + char2;
 
-      let obj1 = {
-        content: "" + char1 + "+" + char2 + "",
-        result: result,
-        isDiscovered:false
-      };
+        let obj1 = {
+          content: "" + char1 + "+" + char2 + "",
+          result: result,
+          isDiscovered: false,
+        };
 
-      let obj2 = {
-        content: "" + result + "",
-        result: result,
-        isDiscovered:false
-      };
+        let obj2 = {
+          content: "" + result + "",
+          result: result,
+          isDiscovered: false,
+        };
 
-      this.cards.push(obj1);
-      this.cards.push(obj2);
+        this.cards.push(obj1);
+        this.cards.push(obj2);
+      }
+
+      localStorage.setItem("LIBRAS_MATH_CARDS_DATA", JSON.stringify(this.cards));
     }
   },
   methods: {
@@ -60,22 +66,24 @@ export default {
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min + 1)) + min;
     },
-    verifyResult: function(card){
-        if(card.complete){
-            return;
-        }else if(this.card1 === ''){
-            this.card1 = card;
-            this.cards[this.cards.indexOf(card)].isDiscovered = true;
-        }else if(this.card1.result === card.result){
-            this.cards[this.cards.indexOf(card)].isDiscovered = true;
-            this.cards[this.cards.indexOf(card)].complete = true;
-            this.cards[this.cards.indexOf(this.card1)].complete = true;
-            this.card1 = ''
-        }else{
-            this.cards[this.cards.indexOf(this.card1)].isDiscovered = false;
-            this.card1 = ''
-        }
-    }
+    verifyResult: function (card) {
+      if (card.complete) {
+        return;
+      } else if (this.card1 === "") {
+        this.card1 = card;
+        this.cards[this.cards.indexOf(card)].isDiscovered = true;
+      } else if (this.card1.result === card.result) {
+        this.cards[this.cards.indexOf(card)].isDiscovered = true;
+        this.cards[this.cards.indexOf(card)].complete = true;
+        this.cards[this.cards.indexOf(this.card1)].complete = true;
+        this.card1 = "";
+      } else {
+        this.cards[this.cards.indexOf(this.card1)].isDiscovered = false;
+        this.card1 = "";
+      }
+      
+      localStorage.setItem("LIBRAS_MATH_CARDS_DATA", JSON.stringify(this.cards));
+    },
   },
 };
 </script>
