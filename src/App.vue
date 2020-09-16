e8bde70a6fd9b72a19c9f48ad6838773773ee2b2
Paulo Henrique <phvcandido@gmail.com>
2020-09-15 21:13:55 -0300

    Adicionando bulma e node-sass

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Início</router-link>|
+    <router-link to="/sobre">Sobre</router-link>|
+    <router-link to="/pontuacao">Minha pontuação</router-link>|
+    <router-link to="/pontuacao">Jogar</router-link>
   </div>
-  <router-view/>
+  <div class="container">
+    <div class="notification">
+      <router-view />
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss">
+@import "assets/sass/global.scss";
 </style>
