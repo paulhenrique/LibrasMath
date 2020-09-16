0ce6a0868e7a922ab79c4fd4940f1ea74fa38630
Paulo Henrique <phvcandido@gmail.com>
2020-09-15 22:30:46 -0300

    Ajustando responsividade

--- src/pages/About/index.vue
+++ src/pages/About/index.vue
@@ -1,8 +1,35 @@
 <template>
-    <h1>About LibrasMath</h1>
+  <div class="about">
+    <header>
+    <router-link class="back-icon-btn" to="/"><img src="/img/icons/left-header-icon.png" alt="Botão de voltar"></router-link>
+      <h1>Como jogar?</h1>
+      <h2>Aqui estão as perguntas e respostas mais comuns do app</h2>
+    </header>    
+    <section>
+      <div class="row">
+        <h1>O que é o HandsOn?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veritatis voluptatum eos, nam unde quod aspernatur hic repellendus harum similique porro delectus pariatur in quibusdam vel optio ipsum quas quasi!</p>
+      </div>
+      <div class="row">
+        <h1>O que é o HandsOn?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veritatis voluptatum eos, nam unde quod aspernatur hic repellendus harum similique porro delectus pariatur in quibusdam vel optio ipsum quas quasi!</p>
+      </div>
+      <div class="row">
+        <h1>O que é o HandsOn?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veritatis voluptatum eos, nam unde quod aspernatur hic repellendus harum similique porro delectus pariatur in quibusdam vel optio ipsum quas quasi!</p>
+      </div>
+      <div class="row">
+        <h1>O que é o HandsOn?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veritatis voluptatum eos, nam unde quod aspernatur hic repellendus harum similique porro delectus pariatur in quibusdam vel optio ipsum quas quasi!</p>
+      </div>
+    </section>
+  </div>
 </template>
 <script>
 export default {
-    name:'About'
-}
+  name: "About",
+};
 </script>
+<style lang="scss">
+@import "./style.scss";
+</style>
