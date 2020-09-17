66f6171e2351fb8ff08650beb0697ed2f647a72e
Paulo Henrique <phvcandido@gmail.com>
2020-09-17 11:10:45 -0300

    Adicionando fases

--- src/pages/Play/index.vue
+++ src/pages/Play/index.vue
@@ -19,13 +19,16 @@
         :class="card.isDiscovered? 'active' : ''"
       >
         <div class="contain" v-html="card.content"></div>
-        <!-- <img :src="'/img/numeros_libras/' + card.result + '.PNG'"> -->
       </div>
     </div>
     <footer>
       <button @click="cleanCurrentFase" class="left">Fase anterior</button>
       <button @click="cleanCurrentFase" class="clean">Reiniciar fase</button>
-      <button :disabled="faseCompleta" @click="cleanCurrentFase" class="next disabled">Próxima fase</button>
+      <button
+        @click="nextFase"
+        class="next"
+        :class="!faseCompleta? 'disabled': ''"
+      >Próxima fase</button>
     </footer>
   </div>
 </template>
@@ -37,91 +40,141 @@ export default {
     card1: "",
     score: 0,
     fases: [],
-    faseCompleta: false,
     faseIndex: 0,
   }),
+  computed: {
+    faseCompleta: function () {
+      // let scoreCompare = this.cards.map((entry) => entry.complete);
+
+      // let counter = 0;
+      // scoreCompare.forEach((a) => {
+      //   if (a) {
+      //     counter++;
+      //   }
+      // });
+
+      // return counter == 12? true : false;
+      return true;
+    },
+  },
   mounted() {
-    if (localStorage.getItem("LIBRAS_MATH_CARDS_DATA")) {
-      this.cards = JSON.parse(localStorage.getItem("LIBRAS_MATH_CARDS_DATA"));
-    } else {
-      for (var i = 0; i < 6; i++) {
-        var char1 = this.getRandomIntInclusive(0, 10);
-        var char2 = this.getRandomIntInclusive(0, 10);
-
-        var result = char1 + char2;
-
-        var imgchar1 = "<img src='/img/numeros_libras/" + char1 + ".PNG'>";
-        if(String(char1).length > 1){
-          imgchar1 = "<img src='/img/numeros_libras/" + String(char1)[0] + ".PNG'> <img src='/img/numeros_libras/" + String(char1)[1] + ".PNG'>";
-        }
-
-        var imgchar2 = "<img src='/img/numeros_libras/" + char2 + ".PNG'>";
-        if(String(char2).length > 1){
-          imgchar2 = "<img src='/img/numeros_libras/" + String(char2)[0] + ".PNG'> <img src='/img/numeros_libras/" + String(char2)[1] + ".PNG'>";
-        }
-        
-        let obj1 = {
-          content: imgchar1 + "+" + imgchar2 + "",
-          char1,
-          char2,
-          result: result,
-          isDiscovered: false,
-        };
-
-        var imgResult = "<img src='/img/numeros_libras/" + result + ".PNG'>";
-        if(String(result).length > 1){
-          imgResult = "<img src='/img/numeros_libras/" + String(result)[0] + ".PNG'> <img src='/img/numeros_libras/" + String(result)[1] + ".PNG'>";
-        }
-        let obj2 = {
-          content: imgResult,
-          result: result,
-          isDiscovered: false,
-        };
-
-        this.cards.push(obj1);
-        this.cards.push(obj2);
-      }
-
-      this.cards = this.cards.map((a) => ({sort: Math.random(), value: a})).sort((a, b) => a.sort - b.sort).map((a) => a.value);
-      localStorage.setItem(
-        "LIBRAS_MATH_CARDS_DATA",
-        JSON.stringify(this.cards)
-      );
-
-    }
-
-    if (localStorage.getItem("LIBRAS_MATH_SCORE_DATA")) {
-      this.score = Number(localStorage.getItem("LIBRAS_MATH_SCORE_DATA"));
-    } else {
-      localStorage.setItem("LIBRAS_MATH_SCORE_DATA", this.score);
-    }
-
-    if (localStorage.getItem("LIBRAS_MATH_FASES_DATA")) {
-      this.fases = JSON.parse(localStorage.getItem("LIBRAS_MATH_FASES_DATA"));
-      this.faseIndex = this.fases.indexOf(this.cards);
-      this.fases.forEach( (content,index) => {
-        if(JSON.stringify(content) == JSON.stringify(this.cards)){
-          this.faseIndex = index;
-        }
-      })
-    } else {
-      this.fases.push(this.cards);
-      this.fases.forEach( (content,index) => {
-        if(JSON.stringify(content) == JSON.stringify(this.cards)){
-          this.faseIndex = index;
-        }
-      })
-      localStorage.setItem(
-        "LIBRAS_MATH_FASES_DATA",
-        JSON.stringify(this.fases)
-      );
-    }
+    this.getCards();
+    this.getScore();
+    this.getFase();
   },
   methods: {
+    nextFase: function () {
+      if (!this.faseCompleta) return;
+      this.cards = [];
+      this.generateCards();
+      this.fases.push(this.cards);
+      localStorage.setItem(
+          "LIBRAS_MATH_FASES_DATA",
+          JSON.stringify(this.fases)
+        );
+      this.verifyFaseIndex();
+    },
+    getCards: function () {
+      if (localStorage.getItem("LIBRAS_MATH_CARDS_DATA")) {
+        this.cards = JSON.parse(localStorage.getItem("LIBRAS_MATH_CARDS_DATA"));
+      } else {
+        this.generateCards();
+      }
+    },
+    generateCards:function(){
+            for (var i = 0; i < 6; i++) {
+          var char1 = this.getRandomIntInclusive(0, 10);
+          var char2 = this.getRandomIntInclusive(0, 10);
+
+          var result = char1 + char2;
+
+          var imgchar1 = "<img src='/img/numeros_libras/" + char1 + ".PNG'>";
+          if (String(char1).length > 1) {
+            imgchar1 =
+              "<img src='/img/numeros_libras/" +
+              String(char1)[0] +
+              ".PNG'> <img src='/img/numeros_libras/" +
+              String(char1)[1] +
+              ".PNG'>";
+          }
+
+          var imgchar2 = "<img src='/img/numeros_libras/" + char2 + ".PNG'>";
+          if (String(char2).length > 1) {
+            imgchar2 =
+              "<img src='/img/numeros_libras/" +
+              String(char2)[0] +
+              ".PNG'> <img src='/img/numeros_libras/" +
+              String(char2)[1] +
+              ".PNG'>";
+          }
+
+          let obj1 = {
+            content: imgchar1 + "+" + imgchar2 + "",
+            char1,
+            char2,
+            result: result,
+            isDiscovered: false,
+          };
+
+          var imgResult = "<img src='/img/numeros_libras/" + result + ".PNG'>";
+          if (String(result).length > 1) {
+            imgResult =
+              "<img src='/img/numeros_libras/" +
+              String(result)[0] +
+              ".PNG'> <img src='/img/numeros_libras/" +
+              String(result)[1] +
+              ".PNG'>";
+          }
+          let obj2 = {
+            content: imgResult,
+            result: result,
+            isDiscovered: false,
+          };
+
+          this.cards.push(obj1);
+          this.cards.push(obj2);
+        }
+
+        this.cards = this.cards
+          .map((a) => ({ sort: Math.random(), value: a }))
+          .sort((a, b) => a.sort - b.sort)
+          .map((a) => a.value);
+        localStorage.setItem(
+          "LIBRAS_MATH_CARDS_DATA",
+          JSON.stringify(this.cards)
+        );
+    },
     getRandomIntInclusive: function (min, max) {
       min = Math.ceil(min);
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    getScore: function () {
+      if (localStorage.getItem("LIBRAS_MATH_SCORE_DATA")) {
+        this.score = Number(localStorage.getItem("LIBRAS_MATH_SCORE_DATA"));
+      } else {
+        localStorage.setItem("LIBRAS_MATH_SCORE_DATA", this.score);
+      }
+    },
+    verifyFaseIndex:function(){
+      this.fases.forEach((content, index) => {
+          if (JSON.stringify(content) == JSON.stringify(this.cards)) {
+            this.faseIndex = index;
+          }
+        });
+    },
+    getFase: function () {
+      if (localStorage.getItem("LIBRAS_MATH_FASES_DATA")) {
+        this.fases = JSON.parse(localStorage.getItem("LIBRAS_MATH_FASES_DATA"));
+        this.verifyFaseIndex();
+      } else {
+        this.fases.push(this.cards);
+        this.verifyFaseIndex();
+        localStorage.setItem(
+          "LIBRAS_MATH_FASES_DATA",
+          JSON.stringify(this.fases)
+        );
+      }
     },
     verifyResult: function (card) {
       if (card.complete) {
@@ -153,7 +206,6 @@ export default {
       );
     },
     updateFases: function () {
-      console.log(this.faseIndex);
       this.fases[this.faseIndex] = this.cards;
       localStorage.setItem(
         "LIBRAS_MATH_FASES_DATA",
@@ -164,14 +216,6 @@ export default {
       localStorage.setItem("LIBRAS_MATH_SCORE_DATA", Number(this.score));
     },
     addScore: function () {
-      //   let scoreCompare = this.cards.map((entry) => entry.complete);
-
-      //   let counter = 0;
-      //   scoreCompare.forEach((a) => {
-      //     if (a) {
-      //       counter++;
-      //     }
-      //   });
       this.score = Number(this.score) + 10;
       this.updateScore();
     },
