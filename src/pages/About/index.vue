88a14bf117cfd3408fc3c75669c93ebc0188a130
Kifani <52540821+Kifani@users.noreply.github.com>
2020-09-16 22:50:46 -0300

    Update index.vue

--- src/pages/About/index.vue
+++ src/pages/About/index.vue
@@ -16,7 +16,7 @@
       </div>
       <div class="row">
         <h1>O que é o HandsOn?</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veritatis voluptatum eos, nam unde quod aspernatur hic repellendus harum similique porro delectus pariatur in quibusdam vel optio ipsum quas quasi!</p>
+        <p>Olá!</p>
       </div>
       <div class="row">
         <h1>O que é o HandsOn?</h1>
