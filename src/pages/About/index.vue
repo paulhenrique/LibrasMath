90ce2eade1822edda2ec12ac7c2532b99aa5583a
Kifani <52540821+Kifani@users.noreply.github.com>
2020-09-17 01:15:16 -0300

    Update index.vue

--- src/pages/About/index.vue
+++ src/pages/About/index.vue
@@ -7,12 +7,14 @@
     </header>    
     <section>
       <div class="row">
-        <h1>O que é o HandsOn?</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veritatis voluptatum eos, nam unde quod aspernatur hic repellendus harum similique porro delectus pariatur in quibusdam vel optio ipsum quas quasi!</p>
+        <h1>O que é o LibrasMath?</h1>
+        <p>O LibrasMath é um Progressive Web App para a prática de numerais em LIBRAS e material didático de ensino de matemática para alunos dos anos iniciais do Ensino Fundamental.</p>
       </div>
       <div class="row">
-        <h1>O que é o HandsOn?</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veritatis voluptatum eos, nam unde quod aspernatur hic repellendus harum similique porro delectus pariatur in quibusdam vel optio ipsum quas quasi!</p>
+        <h1>Como Jogar?</h1>
+        <p>Esta é uma adaptação do tradicional jogo da memória, quando clicar em <i>"jogar"</i> os cartões ficaram disponíveis, cada cartão possui um valor ou uma operação e seu objetivo é encontrar os pares. </p>
+        <h2>Teste 2</h2>
+        <p>texto</p>
       </div>
       <div class="row">
         <h1>O que é o HandsOn?</h1>
